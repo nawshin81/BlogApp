74ef5fb119f3099888ed87d9d1f7903394a33a15
nawshin81 <nawshinulfat@iut-dhaka.edu>
2020-10-31 22:50:08 +0600

    app navigation

--- App.js
+++ App.js
@@ -1,21 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-export default function App() {
+import NotificationScreen from "./src/screens/Notification";
+import HomeScreen from "./src/screens/HomeScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+import SignInScreen from "./src/screens/SignInScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
+import CommentScreen from "./src/screens/CommentScreen";
+import { AuthContext, AuthProvider } from "./src/providers/AuthProvider";
+
+const AuthStack = createStackNavigator();
+const CommentStack = createStackNavigator();
+const HomeTab = createMaterialBottomTabNavigator();
+const AppDrawer = createDrawerNavigator();
+import { Entypo, AntDesign, Ionicons } from "@expo/vector-icons";
+
+const AppDrawerScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppDrawer.Navigator>
+      <AppDrawer.Screen name="Home" component={CommentScreenStack}/>
+      <AppDrawer.Screen name="Profile" component={ProfileScreen}/>
+    </AppDrawer.Navigator>
   );
+};
+
+const HomeTabScreen = () => {
+  return (
+    <HomeTab.Navigator initialRouteName="Home">
+      <HomeTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" color="white" size={26} />
+            ) : (
+              <AntDesign name="home" color="white" size={22} />
+            ),
+        }}
+      />
+      <HomeTab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="ios-notifications" size={26} color="white" />
+            ) : (
+              <Ionicons
+                name="ios-notifications-outline"
+                size={22}
+                color="white"
+              />
+            ),
+        }}
+      />
+    </HomeTab.Navigator>
+  );
+};
+const CommentScreenStack=()=>{
+  return(
+    <CommentStack.Navigator initialRouteName="Home">
+      <CommentStack.Screen name="Home" component={HomeTabScreen} options={{ headerShown: false }}/>
+      <CommentStack.Screen name="Comment" component={CommentScreen} options={{ headerShown: false }}/>
+    </CommentStack.Navigator>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const AuthStackScreen = () => {
+  return (
+    <AuthStack.Navigator initialRouteName="SignIn">
+      <AuthStack.Screen
+        name="SignIn"
+        component={SignInScreen}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+    </AuthStack.Navigator>
+  );
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <AuthContext.Consumer>
+        {(auth) => (
+          <NavigationContainer>
+            {auth.IsLoggedIn ? <AppDrawerScreen /> : <AuthStackScreen />}
+          </NavigationContainer>
+        )}
+      </AuthContext.Consumer>
+    </AuthProvider>
+  );
+}
+export default App;
